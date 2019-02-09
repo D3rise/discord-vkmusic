d8d1b1bc582e73443fa26dd2ffaf8be707000ace
Derise <derise2000@gmail.com>
2019-02-09 19:03:49 +0300

    Фикс неожиданной ошибки системы, изменение инструкций по установке в связи с невозможностью запуска программы на некоторых компьютерах

--- main.py
+++ main.py
@@ -11,36 +11,38 @@ app_id = '543726720289734656'
 
 
 def run():
-    rpc_obj = rpc.DiscordIpcClient.for_platform(app_id)
-    vk_session = vk_api.VkApi(token=config['VK']['app_token'])
-    vk = vk_session.get_api()
+    try:
+        rpc_obj = rpc.DiscordIpcClient.for_platform(app_id)
+        vk_session = vk_api.VkApi(token=config['VK']['app_token'])
+        vk = vk_session.get_api()
 
-    
-    print("Приложение было проинициализировано. Запуск через 5 секунд.")
-    time.sleep(5)
-    while True:
-        activity = {
-            "assets": {
-                "large_image": "vk"
+        print("Приложение было проинициализировано. Запуск через 5 секунд.")
+        time.sleep(5)
+        while True:
+            activity = {
+                "assets": {
+                    "large_image": "vk"
+                }
             }
-        }
-        res = vk.users.get(user_ids=config['VK']['id'], fields="status")[0]
+            res = vk.users.get(user_ids=config['VK']['id'], fields="status")[0]
 
-        if "status_audio" not in res:
-            state = "Музыка не воспроизводится"
-            if "details" in activity:
-                activity.pop("details")
+            if "status_audio" not in res:
+                state = "Музыка не воспроизводится"
+                if "details" in activity:
+                    activity.pop("details")
 
-            activity.update({'state': state})
-        else:
-            curr_music = vk.users.get(user_ids=config['VK']['id'], fields="status")[0]['status_audio']
-            state = f"Автор - {curr_music['artist']}"
-            details = f"Трек - {curr_music['title']}"
-            activity.update({'state': state, 'details': details})
+                activity.update({'state': state})
+            else:
+                curr_music = vk.users.get(user_ids=config['VK']['id'], fields="status")[0]['status_audio']
+                state = f"Автор - {curr_music['artist']}"
+                details = f"Трек - {curr_music['title']}"
+                activity.update({'state': state, 'details': details})
 
-        
-        rpc_obj.set_activity(activity)
-        time.sleep(15)
+            rpc_obj.set_activity(activity)
+            time.sleep(15)
+    except OSError:
+        print("fuck. Restarting.")
+        run()
 
 
 if __name__ == "__main__":
